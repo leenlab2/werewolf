1d57fd5a594a16b7f2034accdaf3ad6c508ffdd8
Leen Al Lababidi <leenlab2@gmail.com>
2024-05-10 15:24:41 -0400

    Added run and sneak

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,13 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
-    [SerializeField] private float _speed;
+    [SerializeField] private float _speedBase;
     [SerializeField] private float mouseSensitivity;
 
     private Rigidbody playerBody;
     private Vector2 walkingDirection;
     private Vector2 cameraDirection;
+    private float _speed;
 
     private void Start()
     {
@@ -20,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
 
         walkingDirection = Vector2.zero;
         cameraDirection = Vector2.zero;
+        _speed = _speedBase;
     }
 
     public void UpdatePlayerVelocity(InputAction.CallbackContext ctx)
@@ -37,6 +39,30 @@ public class PlayerMovement : MonoBehaviour
     public void UpdateCameraDirection(InputAction.CallbackContext ctx)
     {
         cameraDirection = ctx.ReadValue<Vector2>() * mouseSensitivity;
+    }
+
+    public void Run(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            _speed = 2.5f * _speedBase;
+        }
+        else if (ctx.canceled)
+        {
+            _speed = _speedBase;
+        }
+    }
+
+    public void Sneak(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            _speed = 0.3f * _speedBase;
+        }
+        else if (ctx.canceled)
+        {
+            _speed = _speedBase;
+        }
     }
 
     private void FixedUpdate()
