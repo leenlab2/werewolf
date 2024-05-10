22a25f7c337141576b0001f280aa2d648e460763
Leen Al Lababidi <leenlab2@gmail.com>
2024-05-10 13:11:28 -0400

    Setup libraries

---
+++ Assets/Scripts/Managers/GameState.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameState
+{
+    // for any state information we need to track
+    // keep variables public get private set
+}
