5239e75b67439fc978edc4061775cddb38408ad4
Leen Al Lababidi <leenlab2@gmail.com>
2024-05-10 19:56:10 -0400

    Added weapon mechanics

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance { get; private set; }
     private HealthPoints healthPoints;
+    private Inventory inventory;
 
     private void Start()
     {
@@ -19,6 +22,7 @@ public class PlayerController : MonoBehaviour
         }
 
         healthPoints = GetComponent<HealthPoints>();
+        inventory = GetComponent<Inventory>();
     }
 
     public void TakeDamage(int damage)
@@ -29,5 +33,28 @@ public class PlayerController : MonoBehaviour
     public void Heal(int amount)
     {
         healthPoints.Heal(amount);
+    }
+
+    private GameObject FindClosestEnemy()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100))
+        {
+            if (hit.transform.gameObject.GetComponent<EnemyAI>())
+            {
+                return hit.transform.gameObject;
+            }
+        }
+
+        return null;
+    }
+
+    public void AttackAttempt(int damage)
+    {
+        GameObject enemy = FindClosestEnemy();
+        if (enemy != null)
+        {
+            enemy.GetComponent<HealthPoints>().TakeDamage(damage);
+        }
     }
 }
