a3a5cf4d74370efbc377232243c87e01700e0e25
Leen Al Lababidi <leenlab2@gmail.com>
2024-05-11 23:34:30 -0400

    Added inventory slots UI interaction

--- Assets/Scripts/Inventory Items/Inventory.cs
+++ Assets/Scripts/Inventory Items/Inventory.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 [System.Serializable] public class Slot
 {
@@ -11,6 +14,9 @@ using UnityEngine.InputSystem;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField] private GameObject UI;
+    [SerializeField] private Color selectedColor;
+    [SerializeField] private Color deselectedColor;
     [SerializeField] protected List<Slot> _slots = new List<Slot>();
     public Slot currentSelected { get; private set; }
 
@@ -45,6 +51,8 @@ public class Inventory : MonoBehaviour
             _slots.Remove(currentSelected);
             currentSelected = _slots.Count > 0 ? _slots[0] : null;
         }
+
+        UpdateInventoryUI();
     }
 
     public void RechargeFlashlight(InputAction.CallbackContext ctx)
@@ -76,5 +84,52 @@ public class Inventory : MonoBehaviour
         {
             currentSelected = slot;
         }
+
+        UpdateInventoryUI();
+    }
+
+    private void UpdateInventoryUI()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Transform slotPanelUI = UI.transform.GetChild(i);
+            Transform itemUI = slotPanelUI.GetChild(0);
+
+            if (i >= _slots.Count || _slots[i].item == null)
+            {
+                itemUI.gameObject.SetActive(false);
+                slotPanelUI.GetComponent<Image>().color = deselectedColor;
+            }
+            else
+            {
+                itemUI.GetComponentInChildren<Image>().sprite = _slots[i].item.icon;
+                itemUI.GetComponentInChildren<TextMeshProUGUI>().text = _slots[i].count.ToString();
+                itemUI.gameObject.SetActive(true);
+
+                if (_slots[i] == currentSelected)
+                {
+                    slotPanelUI.GetComponent<Image>().color = selectedColor;
+                }
+                else
+                {
+                    slotPanelUI.GetComponent<Image>().color = deselectedColor;
+                }
+            }
+        }
+    }
+
+    public void SelectNext(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.performed || _slots.Count == 0) { return; }
+
+        float scrollDirection = ctx.action.ReadValue<float>();
+        if (scrollDirection == 0) { return; }
+
+        int increment = scrollDirection > 0 ? 1 : -1;
+        int idx = _slots.IndexOf(currentSelected);
+        idx = (idx + increment + _slots.Count) % _slots.Count;
+        currentSelected = _slots[idx];
+
+        UpdateInventoryUI();
     }
 }
